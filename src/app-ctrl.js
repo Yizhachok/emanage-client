621f92d295df49fd4fb00faaa6b4de7a24a58d5c
Max <maximaka0@gmail.com>
2015-06-06 08:54:44 +0300

    Fix header bugs and add realtime indicator

--- src/app-ctrl.js
+++ src/app-ctrl.js
@@ -1,14 +1,23 @@
 'use strict';
 
 eManage
-	.controller('AppCtrl',function(User,$mdSidenav){
+	.controller('AppCtrl',function($rootScope,User,$mdSidenav){
 		var App=this;
 
 		App.User=User;
+
 		App.openMainMenu=function(){
 			$mdSidenav('main-menu').open();
 		};
 		App.allowedMainMenu=function(){
 			return User.$auth;
 		};
+
+		App.realtimeConnected=false;
+		$rootScope.$on('socket:connect',function(){
+			App.realtimeConnected=true;
+		});
+		$rootScope.$on('socket:disconnect',function(){
+			App.realtimeConnected=false;
+		});
 	});
