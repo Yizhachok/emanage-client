c2a750fcf8dd0bdbc299b78d8beaade130be0191
Jorevan <mr.zhoreg@gmail.com>
2015-06-08 20:00:45 +0300

    Use $window service

--- src/modules/clipboard.js
+++ src/modules/clipboard.js
@@ -1,8 +1,8 @@
 'use strict';
 
 eManage
-	.factory('Clipboard',function(){
+	.factory('Clipboard',function($window){
 		return function CopyToClipboard(text){
-			window.prompt('Copy to clipboard: Ctrl + C -> Enter',text);
+			$window.prompt('Copy to clipboard: Ctrl + C -> Enter',text);
 		};
 	});
