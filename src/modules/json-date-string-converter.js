cb8f9505818a5ab69852a287f47e53f29ff866e2
Max <maximaka0@gmail.com>
2015-06-08 20:56:39 +0300

    Rewrite jsonDateStringConverter

--- src/modules/json-date-string-converter.js
+++ src/modules/json-date-string-converter.js
@@ -2,18 +2,16 @@
 
 eManage
 	.factory('jsonDateStringConverter',function(){
-		// /^(\d{4}|\+\d{6})(?:-(\d{2})(?:-(\d{2})(?:T(\d{2}):(\d{2}):(\d{2})\.(\d+)(Z|([\-+])(\d{2}):(\d{2}))?)?)?)?$/;
-		var regexIso8601=/\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d+([+-][0-2]\d:[0-5]\d|Z)/;
-
-		return function convertDateStringToDates(input){
-			if(angular.isObject(input)) forEach(input,function(value,key){
-				var match;
-				if(typeof value==='string'&&(match=value.match(regexIso8601))){
-					var milliseconds=Date.parse(match[0]);
-					if(!isNaN(milliseconds)) input[key]=new Date(milliseconds);
+		var regexIso8601=/\d{4}-(?:0[1-9]|1[0-2])-(?:0[1-9]|[12]\d|3[01])T(?:[01]\d|2[0-3]):[0-5]\d:[0-5]\d\.\d+(?:[+-][0-2]\d:[0-5]\d|Z)/;
+		return function jsonDateStringConverter(obj){
+			if(typeof obj==='object'&&obj!==null) for(var key in obj) if(obj.hasOwnProperty(key)){
+				var value=obj[key];
+				if(typeof value==='string'&&regexIso8601.test(value)){
+					value=Date.parse(value);
+					if(!isNaN(value)) obj[key]=new Date(value);
 				}
-				else convertDateStringToDates(value);
-			});
-			return input;
+				else jsonDateStringConverter(value);
+			}
+			return obj;
 		};
 	});
